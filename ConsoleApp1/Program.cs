54470b3f7ec28292ea0ae1df5a874a25ddf3aa96
Patrick Teivas <teivas.2011@mail.ru>
2017-11-27 20:47:52 +0200

    Commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,7 +15,7 @@ namespace ConsoleApp1
         {
             //Ülesanne 1
 
-            Console.WriteLine("\nEsimene Ülesanne \n");
+            Console.WriteLine("Esimene ülesanne");
 
             Console.WriteLine("Sisestage 2 arvu");
             int FirstInput = int.Parse(Console.ReadLine());
@@ -43,14 +43,13 @@ namespace ConsoleApp1
 
             //Ülesanne 2
 
-            Console.WriteLine("\n\nTeine Ülesanne \n");
+            Console.WriteLine("\nTeine ülesanne");
 
             string filepath = Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.FullName, "kttekst.txt"); ;
 
             string failText = File.ReadAllText(filepath); //Text from file to string
             string[] words = failText.Split(' '); //Words to word array
 
-            Console.WriteLine("#1");
             Console.WriteLine("Sõnu failis: " + words.Length);
 
             int i = 0;
@@ -62,7 +61,6 @@ namespace ConsoleApp1
                 }
             }
 
-            Console.WriteLine("#2");
             Console.WriteLine("Väiksemad kui 5 tähte sõnu failis: " + i);
 
             Console.WriteLine("\nPress any key to continue!");
@@ -70,12 +68,11 @@ namespace ConsoleApp1
 
             //Ülesanne 3
 
-            Console.WriteLine("\n\nKolmas Ülesanne \n ");
+            Console.WriteLine("\nKolmas ülesanne");
 
             List<float> esimeneList = new List<float>() { 11, 15, 6, 13, 13, 25, 32, 11, 20, 5, 31, 16, 32, 29, 11, 13, 3, 29, 28, 24 };
             List<float> teineList = new List<float>() { 5, 19, 16, 4, 12, 7, 2, 28, 34, 29, 29, 36, 6, 8, 24, 18, 31, 7, 1, 70 };
 
-            Console.WriteLine("#1");
             Console.Write("Sarnased elemendid on: ");
 
             foreach (int b in esimeneList.Intersect(teineList))
@@ -85,36 +82,85 @@ namespace ConsoleApp1
 
             var kolmasList = esimeneList.Concat(teineList); // esimeneList + teineList = kolmasList
 
-            Console.WriteLine(); // Selleks, et #2 oleks jargmisel real
-            Console.WriteLine("#2");
-            float kolmasMax = kolmasList.Max();
-            Console.WriteLine("Kahe listi suurim arv on: " + kolmasMax);
-
-            Console.WriteLine("#3");
-            float kolmasMin = kolmasList.Min();
-            Console.WriteLine("Kahe listi väikseim on: " + kolmasMin);
+            Console.WriteLine();
+            Console.WriteLine("Kahe listi suurim arv on: " + kolmasList.Max());
+            
+            Console.WriteLine("Kahe listi väikseim arv on: " + kolmasList.Min());
 
             // Esimene
-            float esimeneListKeskmine = esimeneList.Sum() / esimeneList.Count();
-
-
-            Console.Write("#4.1 \n");
-            Console.WriteLine("Esimese listi keskmine on: " + esimeneListKeskmine);
+            Console.WriteLine("Esimese listi keskmine on: " + (esimeneList.Sum() / esimeneList.Count()));
 
             // Teine
-            float teineListKeskmine = teineList.Sum() / teineList.Count();
-
-            Console.Write("#4.2 \n");
-            Console.WriteLine("Teise listi keskmine on: " + teineListKeskmine);
+            Console.WriteLine("Teise listi keskmine on: " + (teineList.Sum() / teineList.Count()));
 
             // Kolmas
-            float kolmasListKeskmine = kolmasList.Sum() / kolmasList.Count();
-
-            Console.WriteLine("#5");
-            Console.WriteLine("Mõlema listi keskmine on: " + kolmasListKeskmine);
-
-            Console.ReadKey();
+            Console.WriteLine("Mõlema listi keskmine on: " + (kolmasList.Sum() / kolmasList.Count()));
         }
     }
 }
 
+/*
+-\-                                                     
+    \-- \-                                                  
+     \  - -\                                                
+      \      \\                                             
+       \       \                                            
+        \       \\                                              
+         \        \\                                            
+         \          \\                                        
+         \           \\\                                      
+          \            \\                                                 
+           \            \\                                              
+           \. .          \\                                  
+            \    .       \\                                 
+             \      .    \\                                            
+              \       .  \\                                 
+              \         . \\                                           
+              \            <=)                                         
+              \            <==)                                         
+              \            <=)                                           
+               \           .\\                                           _-
+               \         .   \\                                        _-//
+               \       .     \\                                     _-_/ /
+               \ . . .        \\                                 _--_/ _/
+                \              \\                              _- _/ _/
+                \               \\                      ___-(O) _/ _/ 
+                \                \                  __--  __   /_ /
+                \                 \\          ____--__----  /    \_
+                 \                  \\       -------       /   \_  \_
+                  \                   \                  //   // \__ \_
+                   \                   \\              //   //      \_ \_ 
+                    \                   \\          ///   //          \__- 
+                    \                -   \\/////////    //            
+                    \            -         \_         //              
+                    /        -                      //                
+                   /     -                       ///                  
+                  /   -                       //                      
+             __--/                         ///
+  __________/                            // |               
+//-_________      ___                ////  |                
+        ____\__--/                /////    |                
+   -----______    -/---________////        |                
+     _______/  --/    \                   |                 
+   /_________-/       \                   |                 
+  //                  \                   /                 
+                       \.                 /                 
+                       \     .            /                 
+                        \       .        /                  
+                       \\           .    /                  
+                        \                /                  
+                        \              __|                  
+                        \              ==/                  
+                        /              //                   
+                        /          .  //                    
+                        /   .  .    //                      
+                       /.           /                       
+                      /            //                       
+                      /           /
+                     /          //
+                    /         //
+                 --/         /
+                /          //
+            ////         //
+         ///_________////
+*/
