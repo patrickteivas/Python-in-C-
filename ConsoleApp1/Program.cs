c8822f5ccd21bc9fc43678e9dd1a7da294f60ffc
Trppyxd <33310226+Trppyxd@users.noreply.github.com>
2017-11-27 20:06:28 +0200

    3 Ül Lisamine

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -55,6 +55,58 @@ namespace ConsoleApp1
             }
             Console.WriteLine("Väiksemad kui 5 tähte sõnu failis: " + i);
 
+            //Ülesanne 3
+
+            Console.WriteLine("\nKolmas ülesanne \n ");
+
+            List<float> esimeneList = new List<float>() { 11, 15, 6, 13, 13, 25, 32, 11, 20, 5, 31, 16, 32, 29, 11, 13, 3, 29, 28, 24 };
+            List<float> teineList = new List<float>() { 5, 19, 16, 4, 12, 7, 2, 28, 34, 29, 29, 36, 6, 8, 24, 18, 31, 7, 1, 70 };
+
+            Console.WriteLine("#1");
+            Console.Write("Sarnased elemendid on: ");
+
+            foreach (int b in esimeneList.Intersect(teineList))
+            {
+                Console.Write(b + ", ");
+            }
+
+            var kolmasList = esimeneList.Concat(teineList); // esimeneList + teineList = kolmasList
+
+            Console.WriteLine(); // Selleks, et #2 oleks jargmisel real
+            Console.WriteLine("#2");
+            float kolmasMax = kolmasList.Max();
+            Console.WriteLine("Kahe listi suurim arv on: " + kolmasMax);
+
+            Console.WriteLine("#3");
+            float kolmasMin = kolmasList.Min();
+            Console.WriteLine("Kahe listi väikseim on: " + kolmasMin);
+
+            // Esimene
+            float esimeneListArv = esimeneList.Count();
+            float esimeneListSum = esimeneList.Sum();
+            float esimeneListKeskmine = esimeneListSum / esimeneListArv;
+
+
+            Console.Write("#4.1 \n");
+            Console.WriteLine("Esimese listi keskmine on: " + esimeneListKeskmine);
+
+            // Teine
+            float teineListArv = teineList.Count();
+            float teineListSum = teineList.Sum();
+            float teineListKeskmine = teineListSum / teineListArv;
+
+            Console.Write("#4.2 \n");
+            Console.WriteLine("Teise listi keskmine on: " + teineListKeskmine);
+
+            // Kolmas
+            float kolmasListArv = kolmasList.Count();
+            float kolmasListSum = kolmasList.Sum();
+            float kolmasListKeskmine = kolmasListSum / kolmasListArv;
+
+            Console.WriteLine("#5");
+            Console.WriteLine("Mõlema listi keskmine on: " + kolmasListKeskmine);
+
+            Console.ReadKey();
         }
     }
 }
